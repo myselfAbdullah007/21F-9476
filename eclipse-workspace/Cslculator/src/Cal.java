8c0c59ac0f040d4dbff23352317621889ca1ea45
myselfabdullah <145034230+myselfabdullah@users.noreply.github.com>
2023-10-13 08:03:53 +0500

    Created main of Cal class

--- eclipse-workspace/Cslculator/src/Cal.java
+++ eclipse-workspace/Cslculator/src/Cal.java
@@ -6,4 +6,15 @@ public class Cal {
     public static int subtract(int num1, int num2) {
         return num1 - num2;
     }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = add(a, b);
+        int difference = subtract(a, b);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
 }
