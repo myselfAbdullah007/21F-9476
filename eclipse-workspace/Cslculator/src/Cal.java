1b2dc2c22c61d1da474ba0c62a61173e57052675
myselfabdullah <145034230+myselfabdullah@users.noreply.github.com>
2023-10-13 08:10:10 +0500

    Added \multiply Function

--- eclipse-workspace/Cslculator/src/Cal.java
+++ eclipse-workspace/Cslculator/src/Cal.java
@@ -1,20 +1,25 @@
 public class Cal {
-    public static int add(int num1, int num2) {
-        return num1 + num2;
-    }
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
 
-    public static int subtract(int num1, int num2) {
-        return num1 - num2;
-    }
+	public static int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
 
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+	public static int multi(int num1, int num2) {
+		return num1 * num2;
+	}
 
-        int sum = add(a, b);
-        int difference = subtract(a, b);
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-    }
+		int sum = add(a, b);
+		int difference = subtract(a, b);
+		int multi = multi(a, b);
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Multiply: " + multi);
+	}
 }
